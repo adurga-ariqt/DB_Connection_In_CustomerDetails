4a5b7846d94cdacac298a9c852152766eca0fade
Durga Anaparthi <Durga Anaparthi@LAPTOP-4UV87UTN>
2022-11-23 12:53:32 +0530

    Add project files.

---
+++ DB_Connection_In_CustomerDetails/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace DB_Connection_In_CustomerDetails
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCustomerId = new System.Windows.Forms.TextBox();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.txtCustomerAdress = new System.Windows.Forms.TextBox();
+            this.txtGender = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCustomerId
+            // 
+            this.txtCustomerId.Location = new System.Drawing.Point(371, 101);
+            this.txtCustomerId.Name = "txtCustomerId";
+            this.txtCustomerId.Size = new System.Drawing.Size(208, 22);
+            this.txtCustomerId.TabIndex = 0;
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(371, 166);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(208, 22);
+            this.txtCustomerName.TabIndex = 1;
+            // 
+            // txtCustomerAdress
+            // 
+            this.txtCustomerAdress.Location = new System.Drawing.Point(371, 239);
+            this.txtCustomerAdress.Name = "txtCustomerAdress";
+            this.txtCustomerAdress.Size = new System.Drawing.Size(208, 22);
+            this.txtCustomerAdress.TabIndex = 2;
+            // 
+            // txtGender
+            // 
+            this.txtGender.Location = new System.Drawing.Point(371, 312);
+            this.txtGender.Name = "txtGender";
+            this.txtGender.Size = new System.Drawing.Size(208, 22);
+            this.txtGender.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(253, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "CustomerId";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(253, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "CustomerName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(253, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "CustomerAdress";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(253, 318);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Gender";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(76, 387);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(222, 387);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(371, 387);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(504, 387);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(678, 387);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGender);
+            this.Controls.Add(this.txtCustomerAdress);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.txtCustomerId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCustomerId;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.TextBox txtCustomerAdress;
+        private System.Windows.Forms.TextBox txtGender;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ DB_Connection_In_CustomerDetails/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DB_Connection_In_CustomerDetails
+{
+    public partial class Form1 : Form
+    {
+        string connetionString = "";
+        SqlConnection sqlCon;
+        SqlCommand cmd;
+        SqlDataAdapter adpt;
+        string query = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                connetionString = "server =LAPTOP-4UV87UTN;Database=Customer;Trusted_Connection=true;";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                query = "Insert into Customerinfo values(" + txtCustomerId.Text + ", '" + txtCustomerName.Text + "',' " + txtCustomerAdress.Text + "','" + txtGender.Text + "')";
+                sqlCon = new SqlConnection(connetionString);
+                cmd = new SqlCommand(query, sqlCon);
+                sqlCon.Open();
+                cmd.ExecuteNonQuery();
+                sqlCon.Close();
+                MessageBox.Show("Customer Information Saved Successfully");
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                query = "Update CustomerInfo Set CustomerName= '" + txtCustomerName.Text + "', CustomerAddress =' " + txtCustomerAdress.Text + " ',CustomerGender = '" + txtGender.Text + "' Where CustomerId= " + txtCustomerId.Text + "";
+
+
+
+                sqlCon = new SqlConnection(connetionString);
+                cmd = new SqlCommand(query, sqlCon);
+                sqlCon.Open();
+                int n = cmd.ExecuteNonQuery();
+                if (n == 0)
+                {
+                    MessageBox.Show("Customer information not found");
+                }
+                else
+                    MessageBox.Show("Customer Information updated  Successfully");
+                sqlCon.Close();
+               ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //private void ClearFields()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                query = "Delete from Customer  Where CustomerId= " + txtCustomerId.Text + "";
+
+
+
+                sqlCon = new SqlConnection(connetionString);
+                cmd = new SqlCommand(query, sqlCon);
+                sqlCon.Open();
+                int n = cmd.ExecuteNonQuery();
+                if (n == 0)
+                {
+                    MessageBox.Show("Customer information not found");
+                }
+
+                if (n == 1)
+                {
+                    MessageBox.Show("customer  Information Deleted Successfully");
+                }
+                sqlCon.Close();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                query = "Select * from CustomerInfo Where CustomerId= " + txtCustomerId.Text + "";
+
+
+
+                sqlCon = new SqlConnection(connetionString);
+                adpt = new SqlDataAdapter(query, sqlCon);
+                sqlCon.Open();
+                DataSet ds = new DataSet();
+                adpt.Fill(ds, "CustomerInfo");
+                sqlCon.Close();
+
+
+
+                if (ds != null && ds.Tables["CustomerInfo"].Rows.Count > 0)
+                {
+                    txtCustomerName.Text = ds.Tables["CustomerInfo"].Rows[0]["CustomerName"].ToString();
+                    txtCustomerAdress.Text = ds.Tables["CustomerInfo"].Rows[0]["CustomerAddress"].ToString();
+                    txtGender.Text = ds.Tables["CustomerInfo"].Rows[0]["CustomerGender"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("No Customer Information Found");
+                    ClearFields();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        
+           {
+                ClearFields();
+           }
+        private void ClearFields()
+        {
+            txtCustomerId.Text = "";
+            txtCustomerName.Text = "";
+            txtCustomerAdress.Text = "";
+            txtGender.Text = "";
+        }
+    }
+}
+
